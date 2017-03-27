7e1a89112462ae7d6a3c6ada2c0618b49211f720
pyez1158 <pyez1158@163.com>
2017-03-27 20:37:23 +0800

    脚本整理

---
+++ docs/truncate/delete_std_mall.sql
@@ -0,0 +1,13 @@
+delete from tmall_category where system_code = 'CD-CCG000007';
+delete from tmall_cart where system_code = 'CD-CCG000007';
+delete from tmall_product where system_code = 'CD-CCG000007';
+delete from tmall_product_specs where system_code = 'CD-CCG000007';
+delete from tmall_order where system_code = 'CD-CCG000007';
+delete from tmall_product_order where system_code = 'CD-CCG000007';
+delete from to2o_store where system_code = 'CD-CCG000007';
+delete from to2o_store_action where system_code = 'CD-CCG000007';
+delete from to2o_store_purchase where system_code = 'CD-CCG000007';
+delete from to2o_store_ticket where system_code = 'CD-CCG000007';
+delete from to2o_user_ticket where system_code = 'CD-CCG000007';
+delete from tsys_config where system_code = 'CD-CCG000007';
+delete from tsys_dict where system_code = 'CD-CCG000007';
