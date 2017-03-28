59520f1c6e43d0d5a388c02a55456650c969803a
pyez1158 <pyez1158@163.com>
2017-03-28 21:11:27 +0800

    脚本整理

--- src/main/java/com/xnjr/mall/enums/EBizType.java
+++ src/main/java/com/xnjr/mall/enums/EBizType.java
@@ -16,8 +16,8 @@ public enum EBizType {
 
     , ZH_O2O("-31", "正汇O2O支付"), CG_GIVE_AMOUNT_DX("80", "菜狗币代销"), CG_GIVE_AMOUNT_DF(
             "81", "积分代发"), CG_O2O_CGB("90", "菜狗O2O菜狗币支付"), CG_O2O_CGGFD("91",
-            "菜狗O2O菜狗币返点人民币"), CG_O2O_CGJF("92", "菜狗O2O积分支付"), CG_O2O_RMB("92",
-            "菜狗O2O人民币支付");
+            "菜狗O2O菜狗币返点人民币"), CG_O2O_RMB("92", "菜狗O2O人民币支付"), CG_O2O_CGJF("93",
+            "菜狗O2O积分支付");
 
     public static Map<String, EBizType> getBizTypeMap() {
         Map<String, EBizType> map = new HashMap<String, EBizType>();
