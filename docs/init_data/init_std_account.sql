799eae94df7905a1c00a71c53c9a790d097574e1
haiqingzheng <leo.zheng@ommo.cn>
2017-04-21 15:08:27 +0800

    update

--- docs/init_data/init_std_account.sql
+++ docs/init_data/init_std_account.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO `tstd_account` (`account_number`,`user_id`,`real_name`,`type`,`status`,`currency`,`amount`,`frozen_amount`,`md5`,`add_amount`,`in_amount`,`out_amount`,`create_datetime`,`last_order`,`system_code`) VALUES ('DZTA2017100000000000000','SYS_USER_DZT','平台','P','0','CNY',0,0,'33b28c846c000b153bc34f7f3563c631',0,0,0,now(),'AJ2017041920073612859','CD-CDZT000009');
 
-INSERT INTO `tstd_company_channel` (`id`,`company_code`,`company_name`,`channel_type`,`status`,`channel_company`,`private_key1`,`private_key2`,`private_key3`,`private_key4`,`private_key5`,`page_url`,`error_url`,`back_url`,`fee`,`remark`,`system_code`) VALUES (15,'CD-CDZT000009','定制通','35','1','1430550302','r2jgDFSdiikklwlllejlwjio3242342n','wx9b874d991d7e50d5','aa1832cf32722e0fb977a6c9f6aafa20',NULL,NULL,NULL,NULL,'http://121.43.101.148:3302/xn-dzt/thirdPay/callback',NULL,NULL,'CD-CDZT000009');
+INSERT INTO `tstd_company_channel` (`id`,`company_code`,`company_name`,`channel_type`,`status`,`channel_company`,`private_key1`,`private_key2`,`private_key3`,`private_key4`,`private_key5`,`page_url`,`error_url`,`back_url`,`fee`,`remark`,`system_code`) VALUES (15,'CD-CDZT000009','定制通','35','1','1429521602','r2jgDFSdiikklwlllejlwjio3242342n','wx9b874d991d7e50d5','aa1832cf32722e0fb977a6c9f6aafa20',NULL,NULL,NULL,NULL,'http://121.43.101.148:3302/xn-dzt/thirdPay/callback',NULL,NULL,'CD-CDZT000009');
 
 
